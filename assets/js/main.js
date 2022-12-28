4eb2ea1b70cf5de18a3383f09196e7ba2f01f079
Deni Sugiarto <sugiarto.deni@gmail.com>
2022-12-28 13:22:22 +0700

    Update 2022-12-28T06:22:19.804Z

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,30 @@
 // Form Validation Bootstrap
 (() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
-  
-            form.classList.add('was-validated')
-        }, false)
-    })
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
 })();
 
-$(document).ready(function() {
-    $('.form-select2').select2();
+$(document).ready(function () {
+  $(".form-select2").select2({
+    placeholder: "Select your suburb",
+    allowClear: true,
+  });
 });
