d919bdc343924d2bcadad75d448fd7c41bd79acf
Deni Sugiarto <sugiarto.deni@gmail.com>
2023-01-03 09:40:37 +0700

    add logout button

--- src/js/main.js
+++ src/js/main.js
@@ -29,25 +29,160 @@ $(document).ready(function () {
   });
 });
 
-const ctx = document.getElementById("oximeter-chart");
+const ctxOximeter = document.getElementById("oximeter-chart");
 
-new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+if (ctxOximeter != null) {
+  const data = {
+    labels: ["Morning", "Afternoon", "Night"],
     datasets: [
       {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1,
+        label: "Oxigen Rate",
+        data: [85, 93, 98],
+        borderWidth: 2,
+        backgroundColor: "rgb(15, 25, 40)",
+        borderColor: "rgb(15, 25, 40)",
+        fill: false,
+      },
+      {
+        label: "Acceptable",
+        data: [100, 100, 100, 100, 100], //max area
+        backgroundColor: "rgba(205, 98, 118, 0)",
+        borderColor: "rgba(205, 98, 118, 0)",
+        fill: { above: "rgba(14, 218, 249, 1)", target: { value: 95 } }, // min area
+      },
+      {
+        label: "Get Advice from healthcare team",
+        data: [95, 95, 95, 95, 95],
+        backgroundColor: "rgba(77, 157, 46, 0)",
+        borderColor: "rgba(77, 157, 46, 0)",
+        fill: { above: "rgba(63, 204, 91, .75)", target: { value: 92 } },
+      },
+      {
+        label: "Urgent, Call 111",
+        data: [92, 92, 92, 92, 92],
+        backgroundColor: "rgba(89, 162, 201, 0)",
+        borderColor: "rgba(89, 162, 201, 0)",
+        fill: { above: "rgba(252, 116, 42, .75)", target: { value: 0 } },
       },
     ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
+  };
+
+  data.labels.unshift("");
+  data.labels.push("");
+  data.datasets[0].data.unshift(null);
+  data.datasets[0].data.push(null);
+
+  new Chart(ctxOximeter, {
+    type: "line",
+    data,
+    options: {
+      maintainAspectRatio: false,
+      scales: {
+        x: {
+          offset: false,
+          grid: {
+            color: (ctxOximeter) => {
+              const lastTickIndex = ctxOximeter.chart.scales.x.ticks.length - 1;
+              if (ctxOximeter.index !== 1 && ctxOximeter.index !== 4) {
+                return "rgba(102,102,102,0.1)";
+              }
+            },
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          position: "bottom",
+          display: false,
+        },
+        tooltip: {
+          backgroundColor: "rgba(255,255,255, 1)",
+          bodyColor: "#101B37",
+          titleColor: "#101B37",
+        },
       },
     },
-  },
+  });
+}
+
+const ctxPulseRate = document.getElementById("pulse-rate-chart");
+
+if (ctxPulseRate != null) {
+  const data = {
+    labels: ["Morning", "Afternoon", "Night"],
+    datasets: [
+      {
+        label: "Pulse Rate",
+        data: [125, 105, 110],
+        borderWidth: 2,
+        backgroundColor: "rgb(15, 25, 40)",
+        borderColor: "rgb(15, 25, 40)",
+        fill: false,
+      },
+      {
+        label: "Acceptable",
+        data: [100, 100, 100, 100, 100], //max area
+        backgroundColor: "rgba(205, 98, 118, 0)",
+        borderColor: "rgba(205, 98, 118, 0)",
+        fill: { above: "rgba(14, 218, 249, 1)", target: { value: 0 } }, // min area
+      },
+      {
+        label: "Get Advice from healthcare team",
+        data: [120, 120, 120, 120, 120],
+        backgroundColor: "rgba(77, 157, 46, 0)",
+        borderColor: "rgba(77, 157, 46, 0)",
+        fill: { above: "rgba(63, 204, 91, .75)", target: { value: 100 } },
+      },
+      {
+        label: "Urgent, Call 111",
+        data: [150, 150, 150, 150, 150],
+        backgroundColor: "rgba(89, 162, 201, 0)",
+        borderColor: "rgba(89, 162, 201, 0)",
+        fill: { above: "rgba(252, 116, 42, .75)", target: { value: 120 } },
+      },
+    ],
+  };
+
+  data.labels.unshift("");
+  data.labels.push("");
+  data.datasets[0].data.unshift(null);
+  data.datasets[0].data.push(null);
+
+  new Chart(ctxPulseRate, {
+    type: "line",
+    data,
+    options: {
+      maintainAspectRatio: false,
+      scales: {
+        x: {
+          offset: false,
+          grid: {
+            color: (ctxPulseRate) => {
+              const lastTickIndex =
+                ctxPulseRate.chart.scales.x.ticks.length - 1;
+              if (ctxPulseRate.index !== 1 && ctxPulseRate.index !== 4) {
+                return "rgba(102,102,102,0.1)";
+              }
+            },
+          },
+        },
+      },
+      plugins: {
+        legend: {
+          position: "bottom",
+          display: false,
+        },
+        tooltip: {
+          backgroundColor: "rgba(255,255,255, 1)",
+          bodyColor: "#101B37",
+          titleColor: "#101B37",
+        },
+      },
+    },
+  });
+}
+
+const picker = new Litepicker({
+  element: document.getElementById("date-litepicker"),
+  singleMode: false,
 });
